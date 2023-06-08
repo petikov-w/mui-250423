112b05f40ada4916a93a8fcb2e3675c01a185a8c
Petikov Vladimir (KROX) <developer-krox@yandex.ru>
2023-06-08 18:33:30 +0300

    080623-1830

--- src/components/CardU.jsx
+++ src/components/CardU.jsx
@@ -13,6 +13,24 @@ const CardBox = styled(Box)`
   }
 `;
 
+const BoxZ = styled(Box)`
+    padding: 0 10px;
+    position: absolute;
+    border-radius: 15px;
+    z-index: 10;
+    bottom: 0;
+    left: 0; 
+    background-color: rgb(139, 129, 129, 0.7);
+    /* display: none; */
+  :hover {    
+      /* transition: 0.8s; */
+      transform: translate(30%, -30%);     
+      display: inline;
+  }
+`;
+
+
+
 // const FilmLength = styled(Box)`
 //     background-color: #ada8a8;      
 //     padding: 0 10px;
@@ -38,7 +56,9 @@ export const CardU = (props, bg) => {
     return (
         <>         
            <Link to={linkFilm} sx={{cursor: 'pointer'}}>    
-             <CardBox sx={{ position: 'relative', zIndex: 1 }}>
+           <Box sx={{ position: 'relative' }}>
+            <CardBox >
+            {/* <CardBox sx={{ position: 'relative', zIndex: 1 }}> */}
                   <CardMedia                  
                     component="img"                    
                     height="350"
@@ -47,8 +67,12 @@ export const CardU = (props, bg) => {
                     sx={{ borderRadius: 2, objectFit: 'fill', minWidth: '210px' ,maxWidth: '230px', 
                           boxShadow: '4px 4px 16px 0px rgba(34, 60, 80, 0.2)'}} 
                   />
+                  <BoxZ>Ghbdtn</BoxZ>
                   { props.typePage === 'top' ? ( <Rating rating={rating} /> ) : ('')}                 
-             </CardBox>                       
+             </CardBox>     
+           </Box>
+             
+                               
             </Link>          
         </>     
     );
