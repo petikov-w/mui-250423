bb331fb9d6bbc83d6ac2ab704fedd028f3d8491e
Petikov Vladimir (KROX) <developer-krox@yandex.ru>
2023-06-10 12:32:46 +0300

    100623-1230

--- src/pages/SerialPage.jsx
+++ src/pages/SerialPage.jsx
@@ -1,14 +1,17 @@
 // import { Helmet } from 'react-helmet';
+import { ListCardsU } from '../components/ListCardsU';
 
 export const SerialPage = () => { 
+    const query = process.env.REACT_APP_API_SERIALS;
     
     return (
         <>
             {/* <Helmet>
                 <title>Cinema Box - Главная</title>
             </Helmet> */}
-                <h2>Сериалы</h2>
-                <p>Это страница сериалов</p>           
+                {/* <h2>Сериалы</h2>
+                <p>Это страница сериалов</p>       */}
+                 <ListCardsU query={query} typePage="top"/>      
         </>
            
     );
