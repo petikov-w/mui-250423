b94931e129d9e35bb4f08f36a00a81f844e05333
Petikov Vladimir (BOSS) <developer-krox@yandex.ru>
2023-07-08 00:05:24 +0300

    070723-2355

--- src/components/FilterSet.jsx
+++ src/components/FilterSet.jsx
@@ -2,65 +2,35 @@
 import {Grid, Autocomplete, TextField, Stack, Chip } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 
-import {FilterTop} from './FilterTop'
+import { FilterTop } from './FilterTop'
+import { FilterSerial } from './FilterSerial';
 
 
 
 export const FilterSet = (props) => {
     
-       const { typePage } = props;
+    const { typePage } = props;
         
-    //    const dispatch = useDispatch();
-    //    const listCountries = useSelector(state => state.listCountries);
-    //    const listGenres = useSelector(state => state.listGenres);
-
-    //    const selectedGenre= useSelector(state=>state.filters.genre);
-    //    const selectedCountrie= useSelector(state=>state.filters.country);
-    //    const selectedCollection= useSelector(state=>state.filters.top_collection);
-    //    console.log('typePage :>> ', typePage);
-    //    console.log('listTop :>> ', listTop);
-    //    console.log('listGenres :>> ', listGenres);
-       
-
-
-
     return (
     <>
          <Grid  container direction="column" alignItems="center" sx={{ width: '20%', ml: 3, mb: 3 }}>       
             <Stack width={220}>            
                 <FilterTop typePage={typePage}/>
-                
+                <FilterSerial typePage={typePage}/>              
 
-             {/* {typePage === 'serial' ? (
-             
-              {JSON.stringify(selectedGenre.genre) === '{}' ?
-                  <Autocomplete sx={{mb: 2 }} size="small" disableClearable={true}                
-                      options={listGenres.genres}
-                      inputValue="" 
-                      getOptionLabel= {(options) => options.genre}         
-                      renderInput={(params)=><TextField {...params} label="Жанр" />}                  
-                      onChange={(event, value) => dispatch({type:'UPDATE_GENRE', payload: value},)}/> : ('') }  
-              {JSON.stringify(selectedCountrie) === '{}' ?
-                  <Autocomplete sx={{ mb: 3 }} size="small" disableClearable={true}
-                      options={listCountries.countrys}
-                      inputValue="" 
-                      getOptionLabel= {(options) => options.country}
-                      renderInput={(params)=><TextField {...params} label="Страна" />}
-                      onChange={(event, value) => dispatch({type:'UPDATE_COUNTRIE', payload: value},)}/>  : ('') }
-             : '')     */}
             </Stack>          
-            <Stack width={220}>
+            {/* <Stack width={220}>
                     <Grid container spacing={1}  direction="row" justifyContent="center">                    
-                {/* {JSON.stringify(selectedGenre) !== '{}' ? <Chip label={`Жанр: ${selectedGenre.genre}`} sx={{mb: 2 }}
-                                                            onDelete={() => dispatch({type:'DELETE_GENRE'})}/> : ''}
-                {JSON.stringify(selectedCountrie) !== '{}' ? <Chip label={`Страна: ${selectedCountrie.country}`} sx={{mb: 2 }}
-                                                            onDelete={() => dispatch({type:'DELETE_COUNTRIE'})}/> : ''}           */}
-                {/* {JSON.stringify(selectedCollection) !== '{}' ? <Chip label={`Коллекция: ${selectedCollection.top}`} 
-                                                            sx={{mb: 2 }}
-                                                            onDelete={() => dispatch({type:'DELETE_TOP_COLLECTION'})}/> : ''}    */}
+                        {JSON.stringify(selectedGenre) !== '{}' ? <Chip label={`Жанр: ${selectedGenre.genre}`} sx={{mb: 2 }}
+                                                                    onDelete={() => dispatch({type:'DELETE_GENRE'})}/> : ''}
+                        {JSON.stringify(selectedCountrie) !== '{}' ? <Chip label={`Страна: ${selectedCountrie.country}`} sx={{mb: 2 }}
+                        
+                        {JSON.stringify(selectedCollection) !== '{}' ? <Chip label={`Коллекция: ${selectedCollection.top}`} 
+                                                                    sx={{mb: 2 }}
+                                                                    onDelete={() => dispatch({type:'DELETE_TOP_COLLECTION'})}/> : ''}  
                     </Grid>
-            </Stack>    
-         </Grid>
-     {/* )  : ('')) */}
+            </Stack>     */}
+         </Grid>     
     </>
-    )};
+    );
+};
